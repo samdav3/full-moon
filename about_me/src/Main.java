c40cd96b35ad995ee110dd187f44cd235615603a
Sam Davenport <sdavenport@students.mchenry.edu>
2023-02-10 17:31:04 -0600

    Initial commit

---
+++ about_me/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Samantha Davenport, but my friends call me Sam.");
+        System.out.println("I have three dogs; a black lab names Ellie, she is 8, a lab pit mix named Lip, he is 5, and an Aussie mix named Dottie, she is 4. ");
+        System.out.println("I am currently working full time for Backstop Solutions Group doing tech support. I hope to one day move up to their engineering department. ");
+        System.out.println("A fun fact about me, I used to sail competitively in the summers. I would travel the midwest and sail in regattas with my cousins!");
+    }
+}
